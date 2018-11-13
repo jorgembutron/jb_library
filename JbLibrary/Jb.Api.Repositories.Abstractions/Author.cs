84d12694f79528cd52e7d6f5aed4c5ce7304d57e
jorgembutron <jorgembutron@hotmail.com>
2018-11-13 00:19:16 -0600

    Add files via upload

---
+++ JbLibrary/Jb.Api.Repositories.Abstractions/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jb.Api.Repositories.Abstractions
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ JbLibrary/Jb.Api.Repositories/LibraryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Jb.Api.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jb.Api.Repositories
+{
+    public class LibraryDbContext : DbContext
+    {
+        private const string ConnectionString =@"Server=(localdb)\mssqllocaldb; Database=JbEfCoreDb; Trusted_Connection=True";
+
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
